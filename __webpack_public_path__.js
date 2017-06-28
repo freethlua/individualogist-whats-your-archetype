56485f5eb46a4d76ac5bad66f0976079eaa1dd8c
freethlua <freethlua@gmail.com>
2017-06-28 19:53:39 +0530

    2017-06-28 19:53:38

--- __webpack_public_path__.js
+++ __webpack_public_path__.js
@@ -2,9 +2,9 @@ if (location.hostname.match(/localhost|127.0.0.1/)) {
   __webpack_public_path__ = '';
   console.log('Webpack public path configured for localhost:', __webpack_public_path__);
 } else if (location.hostname.includes('github.io')) {
-  __webpack_public_path__ = location.pathname.replace(/\/$/,'');
+  __webpack_public_path__ = location.pathname;
   console.log('Webpack public path configured for github:', __webpack_public_path__);
 } else {
-  __webpack_public_path__ = '/wp-content/themes/individualogist/whats-your-archetype';
+  __webpack_public_path__ = '/wp-content/themes/individualogist/whats-your-archetype/';
   console.log('Webpack public path configured for WP:', __webpack_public_path__);
 }
