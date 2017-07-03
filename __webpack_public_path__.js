b910ea9aa257e229f923f06dd867c9637826bb65
freethlua <freethlua@gmail.com>
2017-07-03 18:31:50 +0530

    update isLocalhost

--- __webpack_public_path__.js
+++ __webpack_public_path__.js
@@ -1,6 +1,6 @@
-window.isLocalhost = Boolean(location.hostname.match(/localhost|127.0.0.1/));
+window.isLocalhost = Boolean(location.hostname.match(/localhost|(127|192)\.[0-9.]+/));
 window.isGithub = Boolean(location.hostname.match(/github.io/));
 
-__webpack_public_path__ = isLocalhost ? ''
-  : isGithub ? location.pathname
-  : '/wp-content/themes/individualogist/whats-your-archetype/';
+__webpack_public_path__ = isLocalhost ? '' :
+  isGithub ? location.pathname :
+  '/wp-content/themes/individualogist/whats-your-archetype/';
