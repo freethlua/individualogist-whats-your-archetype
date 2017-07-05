55692f193e377a8c3e251ec022dfa7dea95c3b40
freethlua <freethlua@gmail.com>
2017-07-05 06:46:46 +0530

    fix report intro

--- components/report-intro/index.js
+++ components/report-intro/index.js
@@ -15,18 +15,18 @@ export default props => h.div('.wrapper', {
   h.img('.archetype', { src: require(`../../assets/images/archetype-icons/${props.archetype}.png`) }),
   h.p('.intro', [md(`Dear **${props.archetype}**, we have so much to tell you about who you are, what you're great at, what you're not so great at... But we can't fit everything into a single page. So, we've put everything into a **FREE** archetype reading! We're not expecting anything in return, but it would tickle us pink if you'd be kind enough to share this quiz with your friends and family! If not, just scroll on down to gain instant access to your **FREE** archetype reading!`)]),
   h.div('.box', [
-    h.p([md(`Everything You Need To Know About **${archetypes[props.archetype].title}**... in One Free Archetype Reading`)]),
-    h.p([md(`We've prepared a FREE detailed archetype reading telling you about your love, health, and wealth, just for you!`)]),
+    h.h1([md(`Everything You Need To Know About **${archetypes[props.archetype].title}**... in One Free Archetype Reading`)]),
+    h.h2([md(`We've prepared a FREE detailed archetype reading telling you about your love, health, and wealth, just for you!`)]),
     h.ul([
-      h.li(`Discover your life journey as a Lover and how you can leverage on your strengths to achieve greatness!`),
-      h.li(`Gain valuable insight into your special skills and innate abilities!`),
-      h.li(`Find out how you can use the Lover archetype to overcome anything that life throws at you!`),
+      h.li([md(`Discover your life journey as a **${archetypes[props.archetype].title.replace('The ','')}** and how you can leverage on your strengths to achieve greatness!`)]),
+      h.li([md(`Gain valuable insight into your special skills and innate abilities!`)]),
+      h.li([md(`Find out how you can use **${archetypes[props.archetype].title}** archetype to overcome anything that life throws at you!`)]),
     ]),
-  ]),
-  h.div('.preview', [
-    h.div([h.img({ src: require('../../assets/images/misc/preview-page-3a.jpg') })]),
-    h.div([h.img({ src: require(`../../assets/images/archetypes/covers/${props.archetype}.jpg`) })]),
-    h.div([h.img({ src: require('../../assets/images/misc/preview-page-4a.jpg') })]),
+    h.div('.preview', [
+      h.div([h.img({ src: require('../../assets/images/misc/preview-page-3a.jpg') })]),
+      h.div([h.img({ src: require(`../../assets/images/archetypes/covers/${props.archetype}.jpg`) })]),
+      h.div([h.img({ src: require('../../assets/images/misc/preview-page-4a.jpg') })]),
+    ]),
   ]),
   h.div('.form-conainer', [
     h.p([md(`Instantly Access Your Free Personalized Archetype Reading On **${archetypes[props.archetype].title}** Archetype!`)]),
