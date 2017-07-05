197d33a7570b613b521a8b1fbf793af0c3b9a9c2
freethlua <freethlua@gmail.com>
2017-07-05 10:00:18 +0530

    fix footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -10,7 +10,7 @@ export default h.div('.footer', [
     '[Terms & Conditions](/terms-and-conditions)',
     '[Privacy Policy](/privacy-policy)',
     '[Contact us](/contact-us)',
-    '[Affiliates](/affiliates)',
+    '[Affiliates](/jv-register)',
   ].join(' • ')),
   md(`Copyright ${new Date().getFullYear()} | Individualogist | All Rights Reserved`),
   md('ClickBank is the retailer of products on this site. CLICKBANK® is a registered trademark of Click Sales, Inc., a Delaware corporation located at 917 S. Lusk Street, Suite 200, Boise Idaho, 83706, USA and used by permission. ClickBank’s role as retailer does not constitute an endorsement, approval or review of these products or any claim, statement or opinion used in promotion of these products.'),
