30ef42bafed0f7b0a5cac791c633aeeaf2b96bf6
freethlua <freethlua@gmail.com>
2017-07-07 08:53:51 +0530

    2017-07-07 08:53:51

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -9,94 +9,125 @@ const isDev = process.env.npm_lifecycle_script.includes('webpack-dev-server');
 
 export default {
   context: __dirname.join('src'),
-  entry: { app: ['./__webpack_public_path__', 'whatwg-fetch', 'babel-polyfill', './app'] },
+  entry: {
+    app: [
+      './__webpack_public_path__',
+      'whatwg-fetch',
+      'babel-polyfill',
+      './app'
+    ]
+  },
   output: {
     filename: isDev ? '[name].js' : '[name].[chunkhash].js',
     chunkFilename: 'chunks/[name].[chunkhash].js',
     path: __dirname.join('build'),
     sourceMapFilename: '[file].map',
-    hashDigestLength: 5,
+    hashDigestLength: 5
   },
   devtool: isDev ? 'cheap-module-source-map' : 'source-map',
   module: {
-    rules: [{
-      test: /\.js$/,
-      include: __dirname.join('src'),
-      use: [
-        isDev && 'webpack-module-hot-accept',
-        'babel-loader'
-      ].filter(Boolean)
-    }, {
-      test: /\.css$/,
-      // use: ['style-loader', 'css-loader'],
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [{
-          loader: 'css-loader',
-          options: {
-            minimize: true,
-            sourceMap: true,
-          },
-        }],
-      }),
-    }, {
-      test: /\.styl$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [{
-          loader: 'css-loader',
-          options: {
-            modules: true,
-            // localIdentName: '[hash:base64:5]',
-            localIdentName: '[local]_[hash:base64:5]',
-            // localIdentName: '[local]_[path][name]_[hash:base64:5]',
-            // localIdentName: isDev ? '[path][name]' : '[hash:base64:5]',
-            camelCase: true,
-            minimize: true,
-            sourceMap: true,
-          },
-        }, 'stylus-loader']
-      })
-    }, {
-      test: /\.(png|jpe?g|woff|woff2|eot|ttf|svg|pdf|mp3|docx|txt)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      use: {
-        loader: 'url-loader',
-        query: {
-          limit: 1000,
-          name: isDev ? '[path][name].[ext]' : 'assets/[name]-[hash:base64:5].[ext]',
-        },
+    rules: [
+      {
+        test: /\.js$/,
+        include: __dirname.join('src'),
+        use: [isDev && 'webpack-module-hot-accept', 'babel-loader'].filter(
+          Boolean
+        )
       },
-    }, {
-      test: /\.(pug|jade)$/,
-      use: 'pug-loader',
-    }],
+      {
+        test: /\.css$/,
+        // use: ['style-loader', 'css-loader'],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true,
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                // localIdentName: '[hash:base64:5]',
+                localIdentName: '[local]_[hash:base64:5]',
+                // localIdentName: '[local]_[path][name]_[hash:base64:5]',
+                // localIdentName: isDev ? '[path][name]' : '[hash:base64:5]',
+                camelCase: true,
+                minimize: true,
+                sourceMap: true
+              }
+            },
+            'stylus-loader'
+          ]
+        })
+      },
+      {
+        test: /\.(png|jpe?g|woff|woff2|eot|ttf|svg|pdf|mp3|docx|txt)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: {
+          loader: 'url-loader',
+          query: {
+            limit: 1000,
+            name: isDev ?
+              '[path][name].[ext]' :
+              'assets/[name]-[hash:base64:5].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.(pug|jade)$/,
+        use: 'pug-loader'
+      }
+    ]
   },
-  plugins: [!isDev && new CleanWebpackPlugin(['build']),
+  plugins: [
+    !isDev && new CleanWebpackPlugin(['build']),
     new ExtractTextPlugin({
       filename: '[name].[chunkhash].css',
-      disable: isDev,
+      disable: isDev
       // disable: true,
     }),
-    isDev && new webpack.NamedModulesPlugin() || new webpack.HashedModuleIdsPlugin(), !isDev && new webpack.optimize.CommonsChunkPlugin({
-      name: 'node_modules',
-      minChunks: module => module.context.includes('node_modules'),
-    }), !isDev && new webpack.optimize.CommonsChunkPlugin({
-      name: 'webpack',
-    }), !isDev && new html({
-      template: 'shell.html',
-      inject: false,
-    }), !isDev && new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      // mangle: false,
-      comments: false,
-      output: {
-        ascii_only: true,
-      }
-    }), !isDev && new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      openAnalyzer: false,
-      generateStatsFile: true,
-    }),
+    isDev && new webpack.NamedModulesPlugin() ||
+      new webpack.HashedModuleIdsPlugin(),
+    !isDev &&
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'node_modules',
+        minChunks: module => module.context.includes('node_modules')
+      }),
+    !isDev &&
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'webpack'
+      }),
+    !isDev &&
+      new html({
+        template: 'shell.html',
+        inject: false
+      }),
+    !isDev &&
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        // mangle: false,
+        comments: false,
+        output: {
+          ascii_only: true
+        }
+      }),
+    !isDev &&
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false,
+        generateStatsFile: true
+      })
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -107,6 +138,6 @@ export default {
   },
   devServer: {
     host: '0.0.0.0',
-    disableHostCheck: true,
+    disableHostCheck: true
   }
 };
