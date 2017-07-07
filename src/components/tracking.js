08012c1c394c3a391d033c74685dce9ceaff8f99
freethlua <freethlua@gmail.com>
2017-07-08 00:03:08 +0530

    2017-07-08 00:03:08

--- src/components/tracking.js
+++ src/components/tracking.js
@@ -1,7 +1,7 @@
 import h from 'preact-hyperscript-h';
 import archetypes from '../data/archetypes';
 
-export default props => h('.tracking', [
+export default props => h.div('.tracking', [
   //
   props.quizData && props.quizData.archetype && h.img('.clickmagick', {
     src: archetypes[props.quizData.archetype].clickmagick.imgSrc,
