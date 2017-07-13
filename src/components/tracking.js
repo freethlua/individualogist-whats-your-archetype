4e63f717d7e8da403cb8a18ea089f94528f2540b
freethlua <freethlua@gmail.com>
2017-07-13 19:05:13 +0530

    2017-07-13 19:05:12

--- src/components/tracking.js
+++ src/components/tracking.js
@@ -1,24 +1,32 @@
+import { Component } from 'preact';
 import h from 'preact-hyperscript-h';
 import archetypes from '../data/archetypes';
 
 /* quick-fix for making this render only once in the apps life cycle */
 let clickmagickRenderedOnce = false;
 
-export default props => {
-  const components = [];
-
-  if (props.quizData && props.quizData.archetype) {
-    if (clickmagickRenderedOnce) {
-      console.log('Debug:', 'clickmagick already rendered once, skipping this');
-    } else {
-      console.log('Debug:', 'clickmagick being rendered for the first time');
-      clickmagickRenderedOnce = true;
-      const clickmagick = h.img('.clickmagick', {
-        src: archetypes[props.quizData.archetype].clickmagick.imgSrc,
-      });
-      components.push(clickmagick);
-    }
+export default class extends Component {
+  componentWillMount() {
+    this.setState({ clickmagickRenderedOnce });
   }
+  componentDidMount() {
+    this.setState({ clickmagickRenderedOnce });
+  }
+  render() {
+    const components = [];
 
-  return h.div('.tracking', components);
-};
+    if (this.props.quizData && this.props.quizData.archetype) {
+      if (this.state.clickmagickRenderedOnce) {
+        console.log('Debug:', 'clickmagick already rendered once, skipping this');
+      } else {
+        console.log('Debug:', 'clickmagick being rendered for the first time');
+        clickmagickRenderedOnce = true;
+        const clickmagick = h.img('.clickmagick', {
+          src: archetypes[this.props.quizData.archetype].clickmagick.imgSrc,
+        });
+        components.push(clickmagick);
+      }
+    }
+    return h.div('.tracking', components);
+  }
+}
