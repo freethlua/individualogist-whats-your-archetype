8acaea880fc09a8185c8bfc8a7ffc69de7fabd9c
freethlua <freethlua@gmail.com>
2017-07-13 15:54:21 +0530

    fix clickmagick tracking

--- src/components/tracking.js
+++ src/components/tracking.js
@@ -1,9 +1,19 @@
 import h from 'preact-hyperscript-h';
 import archetypes from '../data/archetypes';
 
-export default props => h.div('.tracking', [
-  //
-  props.quizData && props.quizData.archetype && h.img('.clickmagick', {
-    src: archetypes[props.quizData.archetype].clickmagick.imgSrc,
-  }),
-]);
+/* quick-fix for making this render only once in the apps life cycle */
+let clickmagickRenderedOnce = false;
+
+export default props => {
+  const components = [];
+
+  if (props.quizData && props.quizData.archetype && !clickmagickRenderedOnce) {
+    clickmagickRenderedOnce = true;
+    const clickmagick = h.img('.clickmagick', {
+      src: archetypes[props.quizData.archetype].clickmagick.imgSrc,
+    });
+    components.push(clickmagick);
+  }
+
+  return h.div('.tracking', components);
+};
