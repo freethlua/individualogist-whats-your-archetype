5427a1ce9fab3b36ab4d0152373a8685f90421bb
freethlua <freethlua@gmail.com>
2017-07-20 18:19:15 +0530

    2017-07-20 18:19:14

--- src/components/comments/index.js
+++ src/components/comments/index.js
@@ -7,36 +7,9 @@ const h = hs(styles);
 
 export default h(class comments extends Component {
   componentDidMount() {
-    this.update();
-  }
-  componentDidUpdate() {
-    // this.update();
-  }
-  update() {
-    if (window.isDev || this.ref && this.ref.querySelector('iframe')) {
-      return;
+    if (!window.isDev) {
+      window.FB.XFBML.parse(this.ref);
     }
-    console.log('rendering FB comments...');
-    window.FB.XFBML.parse(this.ref);
-    // // window.FB.XFBML.parse();
-    // clearTimeout(this.timeout);
-    // this.timeout = setTimeout(() => {
-    //   console.log('rendering FB comments...');
-    //   window.FB.XFBML.parse(this.ref);
-    //   // setTimeout(() => this.update(), 2000);
-    // }, 2000);
-    // // clearTimeout(this.timeout);
-    // // // this.timeout = setTimeout(() => this.update(), 2000);
-    // // this.timeout = setTimeout(() => {
-    // //   // window.FB.XFBML.parse(this.ref);
-    // //   setTimeout(() => this.update(), 2000);
-    // // }, 2000);
-    // // if (!window.isDev && !this.rendered && !this.ref.querySelector('iframe')) {
-    // //   console.log('rendering FB comments...');
-    // //   window.FB.XFBML.parse(this.ref);
-    // //   setTimeout(() => this.update(), 300);
-    // //   this.rendered = true;
-    // // }
   }
   render() {
     return h.div('.outer', {
