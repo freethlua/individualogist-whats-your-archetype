240cd0163901406f743b77a449ff86e35ce85788
freethlua <freethlua@gmail.com>
2017-07-26 14:11:18 +0530

    [feat/husky] Add '[branchName]' to commit messages

---
+++ commitmsg.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+const file = process.env.GIT_PARAMS;
+let message = fs.readFileSync(file, 'utf8');
+
+const branch = execSync('git rev-parse --abbrev-ref HEAD', { encoding: 'utf8' }).split(/[\n\r]+/)[0];
+if (!['master'].includes(branch) && !message.includes(branch)) {
+  const prefix = `[${branch}]`;
+  console.log(`Adding '${prefix}' to commit message...`);
+  message = prefix + ' ' + message;
+}
+
+fs.writeFileSync(file, message, 'utf8');
