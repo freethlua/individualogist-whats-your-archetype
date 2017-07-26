58f355330a6f4b292726e9b5f96aa124c0115346
freethlua <freethlua@gmail.com>
2017-07-26 17:43:43 +0530

    [fix/dev] ".dev" hostname `isDev`

--- src/before.js
+++ src/before.js
@@ -1,6 +1,10 @@
-window.isLocalhost = Boolean(!location.hostname || location.hostname.match(/localhost|(127|192)\.[0-9.]+/));
+window.isLocalhost = Boolean(!location.hostname || location.hostname.match(/localhost|(127|192)\.[0-9.]+|.dev$/));
 window.isGithub = Boolean(location.hostname.match(/github.io/));
 window.isDev = isLocalhost || isGithub;
+
+if (window.isDev) {
+  console.log(`Dev mode`, { isLocalhost, isGithub });
+}
 
 // __webpack_public_path__ = isLocalhost ? '' :
 //   isGithub ? location.pathname :
