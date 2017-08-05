608a55419b25c24bb09a18ad573e2aafeca732ff
freethlua <freethlua@gmail.com>
2017-08-06 00:52:25 +0530

    [issues/38/a] wip

--- src/utils/otranscribe-txt-to-duration-based/index.js
+++ src/utils/otranscribe-txt-to-duration-based/index.js
@@ -5,13 +5,14 @@ module.exports = txt => {
 
   let prev
   return json.map(c => {
+    let ret
     if (prev && prev.end) {
-      return `${formatTime(c.end - prev.end)}: ${c.text}`;
+      ret = `${formatTime(c.end - prev.end)}: ${c.text}`;
     } else {
-      return `${formatTime(c.start - (prev && prev.start || 0))}: ${c.text}`;
+      ret = `${formatTime(c.start - (prev && prev.start || 0))}: ${c.text}`;
     }
-    // console.log(i);
     prev = c
+    return ret;
   }).join('\n');
 
 }
@@ -23,6 +24,6 @@ function formatTime(secs) {
   } else if (secs < 60 * 60) {
     const mins = parseInt(secs / 60, 10);
     secs = secs % 60;
-    return `${mins}:${secs}.1000`
+    return `${mins}:${secs}.000`
   }
 }
