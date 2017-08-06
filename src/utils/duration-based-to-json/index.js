d05e58b34a28ac136105af5173a93aa23ed2c2d7
freethlua <freethlua@gmail.com>
2017-08-06 07:39:05 +0530

    [issues/38/a] parseFloat

--- src/utils/duration-based-to-json/index.js
+++ src/utils/duration-based-to-json/index.js
@@ -1,14 +1,14 @@
 module.exports = parseStr;
 
 function parseStr(str) {
-  const pattern = /([0-9])\.([0-9]+): /g;
+  const pattern = /([0-9]\.[0-9]+): /g;
   let match, prev, array = [],
     prevArrItem;
   while (match = pattern.exec(str)) {
     if (prev) array.push({
-      start: (prevArrItem && prevArrItem.start || 0) + parseInt(prev[1], 10) * 1000 + parseInt(prev[2], 10),
+      start: parseFloat(((prevArrItem && prevArrItem.start || 0) + parseFloat(prev[1])).toFixed(3)),
       text: str.substring(prev.index + match[0].length, match.index),
-    });
+    })
     prevArrItem = array[array.length - 1];
     prev = match;
   }
