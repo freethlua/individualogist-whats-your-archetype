54880eae1ca27d4e29030ec7e9fab1f3d8a7b525
freethlua <freethlua@gmail.com>
2017-08-07 14:50:58 +0530

    [issues/47/popup4] focus the button

--- src/components/slider-pause-popup/index.js
+++ src/components/slider-pause-popup/index.js
@@ -6,6 +6,9 @@ import md from 'preact-markdown';
 const h = hs(styles);
 
 export default class extends Component {
+  componentDidMount() {
+    this.buttonEl.focus();
+  }
   render(props) {
     if (!props.show) return;
     if (!props.type || props.type === 'ok') {
@@ -14,7 +17,8 @@ export default class extends Component {
         h.p(props.message),
         h.img('.arrow', { src: require('./assets/double-down-arrows.png') }),
         h.button({
-          onclick: props.done
+          onclick: props.done,
+          ref: _ => this.buttonEl = _,
         }, [props.button || props.buttonLabel]),
       ]);
     } else {
