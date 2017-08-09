42e5413eae9b3c14dd0b1c9b371f523a59a43af0
freethlua <freethlua@gmail.com>
2017-08-09 15:25:12 +0530

    [issues/47/popup/fixes1] fix export-all

--- src/components/report-free/methods/index.js
+++ src/components/report-free/methods/index.js
@@ -1,7 +1,7 @@
-const reqAll = require.context('.', true, /\.js$/);
-const methods = reqAll.keys().reduce((all, name) => ({ ...all, ...reqAll(name) }), {});
+const reqAll = require.context('.');
+const all = reqAll.keys().reduce((all, name) => ({ ...all, ...reqAll(name) }), {});
 
 export default target => {
-  Object.assign(target.prototype, methods);
+  Object.assign(target.prototype, all);
   return target;
 };
