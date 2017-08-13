447f1ebc8e258a80a6eb11e4e30d48f42b37dc7f
freethlua <freethlua@gmail.com>
2017-08-13 17:24:59 +0530

    [staging] fix-subdomain

--- src/components/reading-base/methods/onbeforeunload.js
+++ src/components/reading-base/methods/onbeforeunload.js
@@ -1,3 +1,5 @@
+import fixSubdomain from '../../../utils/fix-subdomain';
+
 export function onbeforeunload(e) {
   if (window.isDev) {
     return;
@@ -10,7 +12,6 @@ export function onbeforeunload(e) {
   }
   setTimeout(() => {
     this.redirectInitiated = true;
-    // location.assign(fixSubdomain(`/deluxe-archetype-report-${this.archetype}-reading-3/?name=${this.props.formData.name}&email=${this.props.formData.email}`));
     const redirectUrl = fixSubdomain(`/deluxe-archetype-report-${this.archetype}-reading-3/?name=${this.props.formData.name}&email=${this.props.formData.email}`);
     const dialogText = 'CLAIM YOUR $10 DISCOUNT NOW\nGET THE DELUXE ARCHETYPE REPORT TODAY!';
     if (confirm(dialogText)) {
@@ -18,10 +19,7 @@ export function onbeforeunload(e) {
     } else {
       this.redirectInitiated = false;
     }
-    location.assign(redirectUrl);
-    // route(`/deluxe-archetype-report-${this.archetype}-reading-3/`);
-  }, 1000);
-  // const dialogText = 'CLAIM YOUR $10 DISCOUNT NOW\nGET THE DELUXE ARCHETYPE REPORT TODAY!';
+  });
   const dialogText = '********************************\n\nATTENTION!!!! \n\nDo not leave this page\n\nClaim your exclusive offer today\n\n\********************************';
   e.returnValue = dialogText;
   return dialogText;
