2e9701d448be180edbf8f47db4d4381d5455440a
freethlua <freethlua@gmail.com>
2017-08-16 00:41:07 +0530

    [issues/56/redirect] fix exit popup

--- src/components/reading-base/methods/onbeforeunload.js
+++ src/components/reading-base/methods/onbeforeunload.js
@@ -2,7 +2,7 @@ import fixSubdomain from '../../../utils/fix-subdomain';
 
 export function onbeforeunload(e) {
   if (window.isDev) {
-    return;
+    // return;
   }
   if (this.redirectInitiated) {
     return;
@@ -10,16 +10,12 @@ export function onbeforeunload(e) {
   if (this.justClicked) {
     return;
   }
-  setTimeout(() => {
-    this.redirectInitiated = true;
-    const redirectUrl = fixSubdomain(`/deluxe-archetype-report-${this.props.quizData.archetype}-reading-3/?name=${this.props.formData.name}&email=${this.props.formData.email}`);
-    const dialogText = 'CLAIM YOUR $10 DISCOUNT NOW\nGET THE DELUXE ARCHETYPE REPORT TODAY!';
-    if (confirm(dialogText)) {
-      location.assign(redirectUrl);
-    } else {
-      this.redirectInitiated = false;
-    }
-  });
+
+  const redirectUrl = fixSubdomain(`/deluxe-archetype-report-${this.props.quizData.archetype}-reading-3/?name=${this.props.formData.name}&email=${this.props.formData.email}`);
+
+  this.redirectInitiated = true;
+  setTimeout(() => location.assign(redirectUrl));
+
   const dialogText = '********************************\n\nATTENTION!!!! \n\nDo not leave this page\n\nClaim your exclusive offer today\n\n\********************************';
   e.returnValue = dialogText;
   return dialogText;
