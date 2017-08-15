c491fce790283be82b337f283aa7439779dd888f
freethlua <freethlua@gmail.com>
2017-08-16 02:09:03 +0530

    [issues/59/new-questions/initial] added

--- src/data/questions.js
+++ src/data/questions.js
@@ -53,7 +53,7 @@ module.exports = [{
     { answer: `Stubborn and strong-willed`, points: { hero: 2, innocent: 1 } },
   ],
 }, {
-  question: `5. We're almost done! If you wanted to change the world, you would focus on:`,
+  question: `5. Now, if you wanted to change the world, you would focus on:`,
   answers: [
     { answer: `Improving existing policies and regulations`, points: { ruler: 2, sage: 1 } },
     { answer: `Creating solutions to solve the world's problems`, points: { creator: 2, magician: 1 } },
@@ -63,7 +63,7 @@ module.exports = [{
     { answer: `Praying and advocating for universal peace`, points: { member: 2, lover: 1 } },
   ],
 }, {
-  question: `6. Last one! Which of the following gives you the greatest sense of fulfillment?`,
+  question: `6. Which of the following gives you the greatest sense of fulfillment?`,
   answers: [
     { answer: `Travelling and visiting as many countries as I can`, points: { jester: 2, explorer: 1 } },
     { answer: `Mastering a new skill or acquiring new knowledge`, points: { magician: 2, creator: 1 } },
@@ -72,4 +72,44 @@ module.exports = [{
     { answer: `Volunteering for charitable causes`, points: { sage: 2, caregiver: 1 } },
     { answer: `Challenging societal expectations and conventions`, points: { outlaw: 2, ruler: 1 } },
   ],
-}];
+}, {
+  question: `7. Let's talk about your weaknesses, which of the following options resembles your greatest weakness?`,
+  answers: [
+    { answer: `I'm too much of a perfectionist`, points: { creator: 2, sage: 1 } },
+    { answer: `I tend to manipulate others`, points: { ruler: 2, magician: 1 } },
+    { answer: `I try too hard to please others`, points: { caregiver: 2, hero: 1 } },
+    { answer: `I place my trust in others too easily`, points: { innocent: 2, lover: 1 } },
+    { answer: `I find it difficult to take things seriously`, points: { explorer: 2, jester: 1 } },
+    { answer: `I can be a little bit too impulsive`, points: { member: 2, outlaw: 1 } },
+  ],
+}, {
+  question: `8. This might be a little bit uncomfortable. Out of all of the options, which are you the most afraid of?`,
+  answers: [
+    { answer: `Rejection`, points: { jester: 2, caregiver: 1 } },
+    { answer: `Being used`, points: { lover: 2, innocent: 1 } },
+    { answer: `Losing control`, points: { magician: 2, ruler: 1 } },
+    { answer: `Failure`, points: { hero: 2, creator: 1 } },
+    { answer: `Commitment`, points: { outlaw: 2, explorer: 1 } },
+    { answer: `Change`, points: { sage: 2, member: 1 } },
+  ],
+}, {
+  question: `9. That's an intriguing fear to have. What about your relationships? What matters to you the most when you're in a relationship?`,
+  answers: [
+    { answer: `Being intimate with each other`, points: { caregiver: 2, lover: 1 } },
+    { answer: `Retaining my freedom`, points: { explorer: 2, outlaw: 1 } },
+    { answer: `Trusting each other`, points: { innocent: 2, magician: 1 } },
+    { answer: `Growing with each other`, points: { creator: 2, sage: 1 } },
+    { answer: `Understanding each other`, points: { hero: 2, jester: 1 } },
+    { answer: `Being committed to each other`, points: { member: 2, ruler: 1 } },
+  ],
+}, {
+  question: `10. Final question! Select the quote that most accurately resembles your outlook on life:`,
+  answers: [
+    { answer: `Do unto others what you want others to do unto you`, points: { sage: 2, caregiver: 1 } },
+    { answer: `When nothing is sure, everything is possible`, points: { magician: 2, explorer: 1 } },
+    { answer: `All for one, and one for all`, points: { ruler: 2, member: 1 } },
+    { answer: `Stand up for what you believe in, even if it means standing alone`, points: { outlaw: 2, hero: 1 } },
+    { answer: `Some days you just have to create your own sunshine`, points: { jester: 2, creator: 1 } },
+    { answer: `Don't stress the could haves. If it should have, it would have`, points: { lover: 2, innocent: 1 } },
+  ],
+}, ];
